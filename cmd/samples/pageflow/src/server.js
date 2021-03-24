390f1d5f2a9db567ee0c5cc64ad3174c55404703
Justin Muller <just@uber.com>
2021-03-24 14:47:34 -0700

    setting up skeleton endpoints

--- cmd/samples/pageflow/src/server.js
+++ cmd/samples/pageflow/src/server.js
@@ -2,7 +2,30 @@ import fastify from 'fastify';
 
 const server = fastify({ logger: true });
 
-server.get('/', async (request, reply) => {
+server.post('/products', async (request, reply) => {
+  // TODO
+
+  // artificial delay
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
+  return { hello: 'world' };
+});
+
+server.get('/products/:productId', async (request, reply) => {
+  // TODO
+
+  // artificial delay
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
+  return { hello: 'world' };
+});
+
+server.put('/products/:productId', async (request, reply) => {
+  // TODO
+
+  // artificial delay
+  await new Promise(resolve => setTimeout(resolve, 2000));
+
   return { hello: 'world' };
 });
 
