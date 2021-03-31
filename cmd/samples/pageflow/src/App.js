3878484c97de6014cb81dfff0fa510bc7c8f4258
Justin Muller <just@uber.com>
2021-03-31 10:09:56 -0700

    small refactor

--- cmd/samples/pageflow/src/App.js
+++ cmd/samples/pageflow/src/App.js
@@ -11,35 +11,31 @@ import {
 } from './routes';
 import './App.css';
 
-const App = () => {
-
-
-  return (
-    <div className="App">
-      <div className="App-header">
-        <Switch>
-          <Route exact path="/products/:productName">
-            <ProductSplitViewPage />
-          </Route>
-          <Route exact path="/products">
-            <CreateProductPage />
-          </Route>
-          <Route exact path="/">
-            <SimpleNavigationPage
-              linkText="Create a new product"
-              linkTo="/products"
-              title="Product home page"
-            />
-          </Route>
-          <Route>
-            <SimpleNavigationPage
-              title="Oops, couldn't find that page."
-            />
-          </Route>
-        </Switch>
-      </div>
+const App = () => (
+  <div className="App">
+    <div className="App-header">
+      <Switch>
+        <Route exact path="/products/:productName">
+          <ProductSplitViewPage />
+        </Route>
+        <Route exact path="/products">
+          <CreateProductPage />
+        </Route>
+        <Route exact path="/">
+          <SimpleNavigationPage
+            linkText="Create a new product"
+            linkTo="/products"
+            title="Product home page"
+          />
+        </Route>
+        <Route>
+          <SimpleNavigationPage
+            title="Oops, couldn't find that page."
+          />
+        </Route>
+      </Switch>
     </div>
-  );
-};
+  </div>
+);
 
 export default App;
