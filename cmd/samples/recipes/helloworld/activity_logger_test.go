e13a97a0f104a24908b1d8b1315cf5648290b626
Quanzheng Long <longquanzheng@users.noreply.github.com>
2021-05-24 13:57:55 -0700

    Add samples of how to test activity (#44)
    * Add samples of how to test activity

---
+++ cmd/samples/recipes/helloworld/activity_logger_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/cadence/activity"
+	"go.uber.org/cadence/testsuite"
+	"go.uber.org/cadence/worker"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"testing"
+)
+
+func sampleActivity(ctx context.Context) error {
+	logger := activity.GetLogger(ctx)
+	logger.Info("test logging")
+	return nil
+}
+
+func Test_Activity_Noop_Logger(t *testing.T) {
+	testSuite := &testsuite.WorkflowTestSuite{}
+	env := testSuite.NewTestActivityEnvironment()
+	env.RegisterActivity(sampleActivity)
+	val, err := env.ExecuteActivity(sampleActivity)
+	require.Nil(t, err)
+	require.True(t, !val.HasValue())
+}
+
+func Test_Activity_Print_Logger(t *testing.T) {
+	testSuite := &testsuite.WorkflowTestSuite{}
+	env := testSuite.NewTestActivityEnvironment()
+
+	logger, err := zap.NewProduction()
+	require.Nil(t, err)
+
+	var outputLogs []string
+	logger = logger.WithOptions(zap.Hooks(
+		func(entry zapcore.Entry) error {
+			outputLogs = append(outputLogs, entry.Message)
+			return nil
+		},
+	))
+
+	env.SetWorkerOptions(worker.Options{
+		Logger: logger,
+	})
+	env.RegisterActivity(sampleActivity)
+
+	val, err := env.ExecuteActivity(sampleActivity)
+
+	require.Nil(t, err)
+	require.True(t, !val.HasValue())
+	require.True(t, len(outputLogs)==1)
+	require.True(t, outputLogs[0] == "test logging")
+}
