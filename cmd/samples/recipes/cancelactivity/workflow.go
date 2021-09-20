66b7758b5e6c7acdd733cf9f43b0237ad1d5efce
Yichao Yang <ycyang@uber.com>
2021-09-20 10:24:40 -0700

    Improve cancel activity sample (#50)
    - Improve cancel activity so that activityTaskCancel event can be shown in history
    - Add unit test
    - Fix makefile for cancel activity

--- cmd/samples/recipes/cancelactivity/workflow.go
+++ cmd/samples/recipes/cancelactivity/workflow.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"time"
 
+	"go.uber.org/cadence"
 	"go.uber.org/cadence/activity"
 	"go.uber.org/cadence/workflow"
 	"go.uber.org/zap"
@@ -18,7 +19,7 @@ import (
 const ApplicationName = "cancelGroup"
 
 // sampleCancelWorkflow workflow decider
-func sampleCancelWorkflow(ctx workflow.Context) error {
+func sampleCancelWorkflow(ctx workflow.Context) (retError error) {
 	ao := workflow.ActivityOptions{
 		ScheduleToStartTimeout: time.Minute,
 		StartToCloseTimeout:    time.Minute * 30,
@@ -30,24 +31,36 @@ func sampleCancelWorkflow(ctx workflow.Context) error {
 	logger.Info("cancel workflow started")
 
 	defer func() {
-		// When workflow is canceled, it has to get a new disconnected context to execute any activities
-		newCtx, _ := workflow.NewDisconnectedContext(ctx)
-		err := workflow.ExecuteActivity(newCtx, cleanupActivity).Get(ctx, nil)
-		if err != nil {
-			logger.Error("Cleanup activity failed", zap.Error(err))
+		if cadence.IsCanceledError(retError) {
+			// When workflow is canceled, it has to get a new disconnected context to execute any activities
+			newCtx, _ := workflow.NewDisconnectedContext(ctx)
+			err := workflow.ExecuteActivity(newCtx, cleanupActivity).Get(ctx, nil)
+			if err != nil {
+				logger.Error("Cleanup activity failed", zap.Error(err))
+				retError = err
+				return
+			}
+			retError = nil
+			logger.Info("Workflow completed.")
 		}
 	}()
 
 	var result string
 	err := workflow.ExecuteActivity(ctx, activityToBeCanceled).Get(ctx, &result)
+	if err != nil && !cadence.IsCanceledError(err) {
+		logger.Error("Error from activityToBeCanceled", zap.Error(err))
+		return err
+	}
 	logger.Info(fmt.Sprintf("activityToBeCanceled returns %v, %v", result, err))
 
+	// Execute activity using a canceled ctx,
+	// activity won't be scheduled and an cancelled error will be returned
 	err = workflow.ExecuteActivity(ctx, activityToBeSkipped).Get(ctx, nil)
-	logger.Error("Error from activityToBeSkipped", zap.Error(err))
+	if err != nil && !cadence.IsCanceledError(err) {
+		logger.Error("Error from activityToBeSkipped", zap.Error(err))
+	}
 
-	logger.Info("Workflow completed.")
-
-	return nil
+	return err
 }
 
 func activityToBeCanceled(ctx context.Context) (string, error) {
@@ -60,7 +73,9 @@ func activityToBeCanceled(ctx context.Context) (string, error) {
 			activity.RecordHeartbeat(ctx, "")
 		case <-ctx.Done():
 			logger.Info("context is cancelled")
-			return "I am canceled by Done", nil
+			// returned canceled error here so that in workflow history we can see ActivityTaskCanceled event
+			// or if not cancelled, return timeout error
+			return "I am canceled by Done", ctx.Err()
 		}
 	}
 }
