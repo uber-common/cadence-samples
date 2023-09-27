6f1da597359d0bdd6d4f90eb9de9ce2852d1f544
Chris Qin <yqin6@ext.uber.com>
2023-09-27 09:37:42 -0700

    Use signal to interrupt cadence worker

--- new_samples/main.go
+++ new_samples/main.go
@@ -1,9 +1,18 @@
 package main
 
-import "github.com/uber-common/cadence-samples/new_samples/worker"
+import (
+	"fmt"
+	"github.com/uber-common/cadence-samples/new_samples/worker"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
-	for {
-		worker.StartWorker()
-	}
+	worker.StartWorker()
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+	fmt.Println("Cadence worker started, press ctrl+c to terminate...")
+	<-done
 }
